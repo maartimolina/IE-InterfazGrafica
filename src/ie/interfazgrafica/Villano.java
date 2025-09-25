3702a3914a9af8062053bdadd3138f749e4ae23f
Martina Molina <percellomar@gmail.com>
2025-09-25 15:21:12 -0300

    se agrego ataque supremo, leviatan del vacio y cambios en villano

--- src/ie/interfazgrafica/Villano.java
+++ src/ie/interfazgrafica/Villano.java
@@ -4,18 +4,27 @@
  */
 package ie.interfazgrafica;
 
+
 /**
  *
  * @author Mar
  */
-    class Villano extends Personaje {
+  class Villano extends Personaje {
+    protected AtaqueSupremo ataqueSupremo;
     public Villano(String nombre, int vida, int fuerza, int defensa, int porcentajeMaldicion) {
         super(nombre, vida, fuerza, defensa, new BendicionDelVacio(), porcentajeMaldicion);
+        if(porcentajeMaldicion == 100){
+            
+            this.ataqueSupremo= new LeviatanDelVacio(this);
+        }
     }
 
     @Override
     public void decidirAccion(Personaje enemigo) {
-        if (!yaInvocoArma) {
+        if(ataqueSupremo != null){
+            ataqueSupremo.ejecutar(enemigo);
+            ataqueSupremo= null;
+        }else if (!yaInvocoArma) {
             invocarArma();
         } else {
             atacar(enemigo);
